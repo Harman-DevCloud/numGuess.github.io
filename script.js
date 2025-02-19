ac74bc5317ea53b1c52ff1a2aaf1e3d1fc911df7
Harman-DevCloud <hmn30004@gmail.com>
2025-02-19 16:36:51 -0700

    Completed

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+  return Math.floor(Math.random()*10);
+}
+
+const compareGuesses = (user,computer,target) => {
+
+  if(Math.abs(target-user) < Math.abs(target-computer)){
+    return true;
+  }else if(Math.abs(target-user) > Math.abs(target-computer)){
+    return false;
+  }else{
+    return true;
+  }
+}
+
+const updateScore = (winner) => {
+  if(winner === 'human'){
+    humanScore +=1;
+  }else{
+    computerScore +=1;
+  }
+}
+
+const advanceRound = () => currentRoundNumber+=1;
